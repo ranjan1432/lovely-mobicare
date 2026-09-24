98440c87d5ae573daf461362ca853e16fe08953e
ranjan1432 <ranjanbalamurugan01@gmail.com>
2026-09-24 14:31:48 +0530

    Configure Cloudinary for production media storage

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -59,6 +59,8 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
+    "cloudinary",
+    "cloudinary_storage",
 
     # Project apps
     "products",
@@ -201,9 +203,15 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
 STORAGES = {
     "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
